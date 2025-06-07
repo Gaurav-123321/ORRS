9661b6c62777bf1e705cad75152b2d2b7951e069
Gaurav Kumar Das <122771317+Gaurav-123321@users.noreply.github.com>
2025-06-08 00:36:43 +0530

    Delete SearchService directory

--- SearchService/src/main/java/com/rms/searchservice/exception/TrainNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.rms.searchservice.exception;
-
-public class TrainNotFoundException extends RuntimeException {
-    public TrainNotFoundException(String message) {
-        super(message);
-    }
-}
