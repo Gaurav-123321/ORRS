dc6e34bfaf2318fb74d1ff89a84d556b67ca5138
Gaurav Kumar Das <122771317+Gaurav-123321@users.noreply.github.com>
2025-06-08 00:36:35 +0530

    Delete BookingService directory

--- BookingService/src/main/java/com/rms/bookingservice/exception/BookingNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.rms.bookingservice.exception;
-
-public class BookingNotFoundException extends RuntimeException{
-	public BookingNotFoundException(String message) {
-		super(message);
-	}
-}
